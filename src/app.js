432a8fb3e7ae732d043e6c615901ce78c5d7c754
Dylan <dylan@Dylans-MacBook-Air.local>
2026-03-10 18:04:51 +1100

    add despatch routes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const receiptAdviceRoutes = require('./routes/receiptAdvice');
-
+const despatchAdviceRoutes = require('./routes/despatchAdvice');
 const app = express();
 app.use(express.json());
 
 connectDB();
 
 app.use('/receipt-advices', receiptAdviceRoutes);
-
+app.use('/despatch-advices',despatchAdviceRoutes);
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
