ddc9d1bbb5ed395efa3ee4b1e303723154d458c8
Dylan <dylan@Dylans-MacBook-Air.local>
2026-03-10 18:06:23 +1100

    added despatch ubl gen

--- src/utils/ublGenerator.js
+++ src/utils/ublGenerator.js
@@ -1,6 +1,5 @@
 const { create } = require('xmlbuilder2');
 
-
 //  Receipt advice data --> UBL 2.1 XML string
 const generateReceiptAdviceXML = (data) => {
   const { receiptAdviceId, dispatchAdviceId, receiptDate, receivedItems, notes } = data;
@@ -43,5 +42,67 @@ const generateReceiptAdviceXML = (data) => {
 };
 
 const formatDate = (date) => new Date(date).toISOString().split('T')[0];
+// Despatch Advice --> UBL 2.1 XML
+const generateDespatchAdviceXML = (data) => {
+  const {
+    despatchAdviceId,
+    externalRef,
+    despatchParty,
+    deliveryParty,
+    dispatchDate,
+    expectedDeliveryDate,
+    items,
+  } = data;
 
-module.exports = { generateReceiptAdviceXML };
+  const doc = create({ version: '1.0', encoding: 'UTF-8' })
+    .ele('DespatchAdvice', {
+      xmlns: 'urn:oasis:names:specification:ubl:schema:xsd:DespatchAdvice-2',
+      'xmlns:cac': 'urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2',
+      'xmlns:cbc': 'urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2',
+    })
+      .ele('cbc:UBLVersionID').txt('2.1').up()
+      .ele('cbc:ID').txt(despatchAdviceId).up()
+      .ele('cbc:IssueDate').txt(formatDate(dispatchDate)).up();
+
+  if (externalRef) {
+    doc.ele('cbc:ReferenceID').txt(externalRef).up();
+  }
+
+  // Despatch party
+  doc
+    .ele('cac:DespatchSupplierParty')
+      .ele('cac:Party')
+        .ele('cbc:ID').txt(despatchParty.partyId).up()
+        .ele('cbc:Name').txt(despatchParty.name).up()
+      .up()
+    .up();
+
+  // Delivery party
+  doc
+    .ele('cac:DeliveryCustomerParty')
+      .ele('cac:Party')
+        .ele('cbc:ID').txt(deliveryParty.partyId).up()
+        .ele('cbc:Name').txt(deliveryParty.name).up()
+      .up()
+    .up();
+
+  if (expectedDeliveryDate) {
+    doc.ele('cbc:ExpectedDeliveryDate').txt(formatDate(expectedDeliveryDate)).up();
+  }
+
+  // Despatch lines
+  items.forEach((item, index) => {
+    doc
+      .ele('cac:DespatchLine')
+        .ele('cbc:ID').txt(String(index + 1)).up()
+        .ele('cbc:DeliveredQuantity', { unitCode: item.uom }).txt(String(item.quantity)).up()
+        .ele('cac:Item')
+          .ele('cbc:Description').txt(item.description || '').up()
+          .ele('cbc:Name').txt(item.sku).up()
+        .up()
+      .up();
+  });
+
+  return doc.end({ prettyPrint: true });
+};
+module.exports = { generateReceiptAdviceXML,generateDespatchAdviceXML, };
