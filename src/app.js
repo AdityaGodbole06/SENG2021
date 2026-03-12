7b547816882d3f96d095500153a70b43807a33c7
musk <tengyuexuan391@gmail.com>
2026-03-12 21:01:24 +1100

    add authentication to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const { authMiddleware } = require('./middleware/auth');
 const receiptAdviceRoutes = require('./routes/receiptAdvice');
 const despatchAdviceRoutes = require('./routes/despatchAdvice');
 const app = express();
@@ -8,8 +9,16 @@ app.use(express.json());
 
 connectDB();
 
-app.use('/receipt-advices', receiptAdviceRoutes);
-app.use('/despatch-advices',despatchAdviceRoutes);
+app.use('/api/despatch-advices', authMiddleware, despatchAdviceRoutes);
+app.use('/api/receipt-advices', authMiddleware, receiptAdviceRoutes);
+
+app.get('/api/test', authMiddleware, (req, res) => {
+    res.status(200).json({
+        message: 'API authentication works',
+        authenticatedParty: req.party
+    });
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
